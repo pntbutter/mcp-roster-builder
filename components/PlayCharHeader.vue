b6cae7f2fb701cdaf1d01d2ea13a5c3e64aaeae2
pntbutter <frallanawsm@gmail.com>
2021-12-04 00:19:31 +0100

    componentize a bit

---
+++ components/PlayCharHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header class="flex  justify-between">
+      <div class="flex  items-center">
+        <div class="w-10  h-10  -ml-5  mr-3  border-2  border-green-500  rounded-full  bg-gray-800">
+
+        </div>
+
+        <h4 class="text-sm">{{ char.name }}</h4>
+      </div>
+
+      <div class="flex  flex-col  items-end  space-y-2">
+        <div class="flex  space-x-3  text-center  text-xs  text-gray-200">
+          <span class="flex  items-center  justify-center "><HeartIcon class="block  h-4  w-4"/> <b class="ml-1">{{ char.stats.healthy.hp }}</b></span>
+          <span class="flex  items-center  justify-center "><ChevronDoubleRightIcon class="block  h-4  w-4"/> <b class="ml-1  uppercase">{{ char.stats.healthy.speed }}</b></span>
+          <span class="flex  items-center  justify-center "><SwitchVerticalIcon class="block  h-4  w-4"/> <b class="ml-1">{{ char.stats.healthy.size }}</b></span>
+        </div>
+
+        <div class="flex  items-center  space-x-2  text-center  text-xs  font-bold  text-black">
+          <span class="text-gray-300  uppercase  tracking-widest">Def:</span>
+          <span class="flex  items-center  justify-center  w-5  h-5  rounded-full  bg-red-500">{{ char.stats.healthy.physical }}</span>
+          <span class="flex  items-center  justify-center  w-5  h-5  rounded-full  bg-yellow-400">{{ char.stats.healthy.energy }}</span>
+          <span class="flex  items-center  justify-center  w-5  h-5  rounded-full  bg-blue-400">{{ char.stats.healthy.mystic }}</span>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import { HeartIcon, ChevronDoubleRightIcon, SwitchVerticalIcon } from '@heroicons/vue/solid'
+
+export default {
+  props: ['char'],
+  components: { HeartIcon, ChevronDoubleRightIcon, SwitchVerticalIcon }
+}
+</script>
--- pages/play.vue
+++ pages/play.vue
@@ -1,34 +1,14 @@
 <template>
   <div v-if="battle" class="max-w-5xl  mx-auto">
-    <div class="md:grid  md:grid-cols-2  gap-6  space-y-10  md:space-y-0  pt-6">
+    <div class="md:grid  md:grid-cols-2  gap-6  space-y-10  md:space-y-0  md:pt-6">
       <div v-if="battle.you">
         <h3 class="mb-4  title  md:text-left">You</h3>
 
         <ul v-if="battle.you.characters" class="space-y-2">
           <li v-for="(char, index) in battle.you.characters" class="card  disable-dbl-tap-zoom">
-            <header class="flex  justify-between">
-              <div>
-
-                <h4>{{ char.name }}</h4>
-              </div>
-
-              <div class="flex  flex-col  items-end  space-y-2">
-                <div class="flex  space-x-3  text-center  text-xs  text-gray-200">
-                  <span class="flex  items-center  justify-center "><HeartIcon class="block  h-4  w-4"/> <b class="ml-1">{{ char.stats.healthy.hp }}</b></span>
-                  <span class="flex  items-center  justify-center "><ChevronDoubleRightIcon class="block  h-4  w-4"/> <b class="ml-1  uppercase">{{ char.stats.healthy.speed }}</b></span>
-                  <span class="flex  items-center  justify-center "><SwitchVerticalIcon class="block  h-4  w-4"/> <b class="ml-1">{{ char.stats.healthy.size }}</b></span>
-                </div>
-
-                <div class="flex  items-center  space-x-2  text-center  text-xs  font-bold  text-black">
-                  <span class="text-gray-300  uppercase  tracking-widest">Def:</span>
-                  <span class="flex  items-center  justify-center  w-5  h-5  rounded-full  bg-red-500">{{ char.stats.healthy.physical }}</span>
-                  <span class="flex  items-center  justify-center  w-5  h-5  rounded-full  bg-yellow-400">{{ char.stats.healthy.energy }}</span>
-                  <span class="flex  items-center  justify-center  w-5  h-5  rounded-full  bg-blue-400">{{ char.stats.healthy.mystic }}</span>
-                </div>
-              </div>
-            </header>
+            <PlayCharHeader :char="char" />
             
-            <footer class="flex  flex-wrap  space-x-6">
+            <footer class="flex  flex-wrap  mt-4  space-x-6">
               <div class="flex  items-center">
                 <button @click="decreasePower(char)" class="flex  items-center  justify-center  w-8  h-8  rounded-full  bg-green-700" :class="{ 'opacity-50': char.state.power <= 0 }">
                   <MinusSmIcon class="block  h-5  w-5"/>
